53225bb241f72841269a086fdcc0850f66140f01
Geert de Pont <geertdepont@yahoo.com>
2016-10-13 15:18:19 +0200

    interpretor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,36 @@ public class Main {
 	
 	// Method to read 1 character.
 	char nextChar (Scanner in) {
-		in.useDelimiter("");
+//		in.useDelimiter("");
 		return in.next().charAt(0);
 	}
 	// Method to check if the next character to be read when
 	// calling nextChar() is equal to the provided character.
-	boolean nextCharIs(Scanner in, char c) {
+	boolean nextCharIs(Scanner in, char c, boolean useEmptyDelimiter) {
+		if(useEmptyDelimiter){
+			in.useDelimiter("");
+		}
+		else{
+			in.reset();
+		}
+		
 		return in.hasNext(Pattern.quote(c+""));
 	}
 	// Method to check if the next character to be read when
 	// calling nextChar() is a digit.
 	boolean nextCharIsDigit (Scanner in) {
+//		in.useDelimiter("");
 		return in.hasNext("[0-9]");
 	}
 	// Method to check if the next character to be read when
 	// calling nextChar() is a letter.
-	boolean nextCharIsLetter (Scanner in) {
+	boolean nextCharIsLetter (Scanner in, boolean useEmptyDelimiter) {
+		if(useEmptyDelimiter){
+			in.useDelimiter("");
+		}
+		else{
+			in.reset();
+		}
 		return in.hasNext("[a-zA-Z]");
 	}
 
@@ -31,18 +45,18 @@ public class Main {
     }
     
     void character (Scanner input, char c) throws APException{
-        if (!nextCharIs(input, c)){
-    	throw new APException("");
+    	if (!nextCharIs(input, c, false)){
+    	throw new APException(c +"is not this character");
         }
         nextChar(input);
     }
     
     boolean nextIsAdditiveOperator(Scanner input){
-    	return nextCharIs(input, '+') || nextCharIs(input, '|') || nextCharIs(input, '-'); 
+    	return nextCharIs(input, '+', false) || nextCharIs(input, '|', false) || nextCharIs(input, '-', false); 
     }
     
     boolean nextIsMultiplicativeOperator(Scanner input){
-    	return nextCharIs(input, '*');
+    	return nextCharIs(input, '*', false);
     }
     
     void program(Scanner input) throws APException{
@@ -54,21 +68,25 @@ public class Main {
     void statement(Scanner input) throws APException{
     	String statement=input.nextLine();
     	Scanner statementScanner=new Scanner(statement);
-    	if(nextCharIsLetter(statementScanner)){
+
+    	if(nextCharIsLetter(statementScanner, true)){
+    		System.out.println("assignment");
     		assignment(statementScanner);
-    	}else if(nextCharIs(statementScanner, '?')){
+    	}else if(nextCharIs(statementScanner, '?', false)){
     		printStatement(statementScanner);
-    	}else if(nextCharIs(statementScanner, '/')){
+     		System.out.println("statement");
+    	}else if(nextCharIs(statementScanner, '/', false)){
     		comment(statementScanner);
+    		System.out.println("comment");
     	}else{
-    		throw new APException("Misformed statement. A statement has to begin with a '?', '/' or an identifier\n");
+    		throw new APException("Misformed statement. A statement has to begin with a '?', '/' or an identifier");
     	}
     }
     
     void assignment(Scanner input) throws APException{
     	Identifier identifier=identifier(input);
     	character(input, '=');
-    	expression(input);
+       	expression(input);
     	eoln(input);
     }
     
@@ -87,13 +105,13 @@ public class Main {
     Identifier identifier(Scanner input) throws APException{
     	StringBuffer identifier = new StringBuffer();
     	
-    	if(nextCharIsLetter(input)){
+    	if(nextCharIsLetter(input, true)){
     		identifier.append(letter(input));
     	}else{
     		throw new APException("identifier has to start with a letter");
     	}
-    	while(nextCharIsLetter(input) || nextCharIsDigit(input)){
-    		if(nextCharIsLetter(input)){
+    	while(nextCharIsLetter(input, true) || nextCharIsDigit(input)){
+    		if(nextCharIsLetter(input, true)){
     			identifier.append(letter(input));
     		}else{
     			identifier.append(number(input));
@@ -104,10 +122,12 @@ public class Main {
 
     void expression(Scanner input) throws APException{
     	term(input);
+    	
     	while(nextIsAdditiveOperator(input)){
     		additiveOperator(input);
     		term(input);
     	}
+    	
     }
     
     void term(Scanner input) throws APException{
@@ -119,14 +139,16 @@ public class Main {
     }
     
     void factor(Scanner input) throws APException{
-	    if(nextCharIsLetter(input)){
+    	System.out.println("problem below here");
+
+    	if(nextCharIsLetter(input, true)){
 			identifier(input);
-		}else if(nextCharIs(input, '(')){
+		}else if(nextCharIs(input, '(', true)){
 			complexFactor(input);
-		}else if(nextCharIs(input, '{')){
+		}else if(nextCharIs(input, '{', true)){
 			set(input);
 		}else{
-			throw new APException("Misformed factor. A factor has to begin with an identifier, '(' or '{'\n");
+			throw new APException("Misformed factor. A factor has to begin with an identifier, '(' or '{'");
 		}
     }
     
@@ -145,7 +167,7 @@ public class Main {
     void rowNaturalNumbers(Scanner input) throws APException{
     	if(nextCharIsDigit(input)){
     		naturalNumber(input);
-    		while(nextCharIs(input, ',')){
+    		while(nextCharIs(input, ',', false)){
     			character(input, ',');
     			naturalNumber(input);
     		}
@@ -153,25 +175,26 @@ public class Main {
     }
     
     char additiveOperator(Scanner input) throws APException{
-    	if(!nextCharIs(input, '+') && !nextCharIs(input, '|') && !nextCharIs(input, '-')){
-    		throw new APException("");
+    	if(!nextCharIs(input, '+', false) && !nextCharIs(input, '|', false) && !nextCharIs(input, '-', false)){
+    		throw new APException("is not additive operator sign");
     	}
     	return nextChar(input);
     }
     
     char multiplicativeOperator(Scanner input) throws APException{
-    	if(!nextCharIs(input, '*')){
-    		throw new APException("");
+    	if(!nextCharIs(input, '*', false)){
+    		throw new APException("is not multiplication sign");
     	}
     	return nextChar(input);
     }
     
     int naturalNumber(Scanner input) throws APException{
-    	return nextCharIs(input, '0') ? zero(input):positiveNumber(input);
+    	return nextCharIs(input, '0', false) ? zero(input):positiveNumber(input);
     }
     
     int positiveNumber(Scanner input) throws APException{
     	StringBuffer result=new StringBuffer();
+    	input.useDelimiter("");
     	result.append(notZero(input));
     	while(nextCharIsDigit(input)){
     		result.append(number(input));
@@ -180,26 +203,26 @@ public class Main {
     }
     
     int number(Scanner input) throws APException{
-    	return nextCharIs(input, '0') ? zero(input) : notZero(input);
+    	return nextCharIs(input, '0', false) ? zero(input) : notZero(input);
     }
     
     int zero(Scanner input) throws APException{
-    	if(!nextCharIs(input, '0')){
-    		throw new APException("");
+    	if(!nextCharIs(input, '0', false)){
+    		throw new APException("is not zero");
     	}
     	return Character.getNumericValue(nextChar(input));
     }
     
 	int notZero(Scanner input) throws APException{
 		if(!input.hasNext("[1-9]")){
-			throw new APException("");
+			throw new APException("is not value bewteen 1-9");
 		}
 		return Character.getNumericValue(nextChar(input));
 	}
     
     char letter(Scanner input) throws APException{
-    	if(!nextCharIsLetter(input)){
-			throw new APException("");
+    	if(!nextCharIsLetter(input, true)){
+			throw new APException("is not a letter");
 		}
     	return nextChar(input);
     }
