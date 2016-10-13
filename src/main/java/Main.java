a1245a4bf1cc52c474aaba252bb673f118016f53
gatisnolv <gatisnolv@gmail.com>
2016-10-13 23:44:01 +0200

    add to parser
    I think the parser is now functioning correctly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,65 +4,72 @@ import java.util.regex.Pattern;
 public class Main {
 	
 	// Method to read 1 character.
-	char nextChar (Scanner in) {
-//		in.useDelimiter("");
+	char nextChar (Scanner in, boolean skipWhiteSpace) {
+		skipWhiteSpace(in,skipWhiteSpace);
 		return in.next().charAt(0);
 	}
 	// Method to check if the next character to be read when
 	// calling nextChar() is equal to the provided character.
-	boolean nextCharIs(Scanner in, char c, boolean useEmptyDelimiter) {
-		if(useEmptyDelimiter){
-			in.useDelimiter("");
-		}
-		else{
-			in.reset();
-		}
-		
+	boolean nextCharIs(Scanner in, char c, boolean skipWhiteSpace) {
+		skipWhiteSpace(in,skipWhiteSpace);
 		return in.hasNext(Pattern.quote(c+""));
 	}
+	
 	// Method to check if the next character to be read when
 	// calling nextChar() is a digit.
-	boolean nextCharIsDigit (Scanner in) {
-//		in.useDelimiter("");
+	boolean nextCharIsDigit (Scanner in, boolean skipWhiteSpace) {
+		skipWhiteSpace(in,skipWhiteSpace);
 		return in.hasNext("[0-9]");
 	}
+	
 	// Method to check if the next character to be read when
 	// calling nextChar() is a letter.
-	boolean nextCharIsLetter (Scanner in, boolean useEmptyDelimiter) {
-		if(useEmptyDelimiter){
-			in.useDelimiter("");
-		}
-		else{
-			in.reset();
-		}
+	boolean nextCharIsLetter (Scanner in, boolean skipWhiteSpace) {
+		skipWhiteSpace(in,skipWhiteSpace);
 		return in.hasNext("[a-zA-Z]");
+	}
+	
+	void skipWhiteSpace(Scanner in, boolean skipWhiteSpace){
+		in.useDelimiter("");
+		if(skipWhiteSpace){
+			while(in.hasNext("\\s+")){
+				in.next().charAt(0);
+			}
+		}
 	}
 
     void eoln(Scanner input) throws APException{
     	if(input.hasNext()){
-    		throw new APException("there is still something here...");
+    		throw new APException("expected end-of-line");
+    	}
+    }
+    
+    void eof(Scanner input) throws APException{
+    	if(input.hasNext()){
+    		throw new APException("expected end-of-file");
     	}
     }
     
     void character (Scanner input, char c) throws APException{
-    	if (!nextCharIs(input, c, false)){
-    	throw new APException(c +"is not this character");
+    	if (!nextCharIs(input, c, true)){
+    	throw new APException("The next character is not "+c);
         }
-        nextChar(input);
+        nextChar(input, true);
     }
     
     boolean nextIsAdditiveOperator(Scanner input){
-    	return nextCharIs(input, '+', false) || nextCharIs(input, '|', false) || nextCharIs(input, '-', false); 
+    	return nextCharIs(input, '+', true) || nextCharIs(input, '|', true) || nextCharIs(input, '-', true); 
     }
     
     boolean nextIsMultiplicativeOperator(Scanner input){
-    	return nextCharIs(input, '*', false);
+    	return nextCharIs(input, '*', true);
     }
     
     void program(Scanner input) throws APException{
-    	while(input.hasNext()){ //Reads the whole file? 
+    	while(input.hasNextLine()){ //Reads the whole file? 
     		statement(input);
     	}
+    	eof(input);//do we need this? (the EBNF implies we need it, but it will never throw the exception)
     }
     
     void statement(Scanner input) throws APException{
@@ -70,14 +77,11 @@ public class Main {
     	Scanner statementScanner=new Scanner(statement);
 
     	if(nextCharIsLetter(statementScanner, true)){
-    		System.out.println("assignment");
     		assignment(statementScanner);
-    	}else if(nextCharIs(statementScanner, '?', false)){
+    	}else if(nextCharIs(statementScanner, '?', true)){
     		printStatement(statementScanner);
-     		System.out.println("statement");
-    	}else if(nextCharIs(statementScanner, '/', false)){
+    	}else if(nextCharIs(statementScanner, '/', true)){
     		comment(statementScanner);
-    		System.out.println("comment");
     	}else{
     		throw new APException("Misformed statement. A statement has to begin with a '?', '/' or an identifier");
     	}
@@ -98,7 +102,9 @@ public class Main {
     
     void comment(Scanner input) throws APException{
     	character(input, '/');
-    	input.nextLine();
+    	if(input.hasNextLine()){//to allow an empty line following the '/' ('/' followed IMMEDIATELY by <eol>)
+    		input.nextLine();
+    	}
     	eoln(input);
     }
     
@@ -110,8 +116,8 @@ public class Main {
     	}else{
     		throw new APException("identifier has to start with a letter");
     	}
-    	while(nextCharIsLetter(input, true) || nextCharIsDigit(input)){
-    		if(nextCharIsLetter(input, true)){
+    	while(nextCharIsLetter(input, false) || nextCharIsDigit(input, false)){
+    		if(nextCharIsLetter(input, false)){
     			identifier.append(letter(input));
     		}else{
     			identifier.append(number(input));
@@ -139,8 +145,6 @@ public class Main {
     }
     
     void factor(Scanner input) throws APException{
-    	System.out.println("problem below here");
-
     	if(nextCharIsLetter(input, true)){
 			identifier(input);
 		}else if(nextCharIs(input, '(', true)){
@@ -165,9 +169,9 @@ public class Main {
     }
     
     void rowNaturalNumbers(Scanner input) throws APException{
-    	if(nextCharIsDigit(input)){
+    	if(nextCharIsDigit(input, true)){
     		naturalNumber(input);
-    		while(nextCharIs(input, ',', false)){
+    		while(nextCharIs(input, ',', true)){
     			character(input, ',');
     			naturalNumber(input);
     		}
@@ -175,56 +179,55 @@ public class Main {
     }
     
     char additiveOperator(Scanner input) throws APException{
-    	if(!nextCharIs(input, '+', false) && !nextCharIs(input, '|', false) && !nextCharIs(input, '-', false)){
+    	if(!nextCharIs(input, '+', true) && !nextCharIs(input, '|', true) && !nextCharIs(input, '-', true)){
     		throw new APException("is not additive operator sign");
     	}
-    	return nextChar(input);
+    	return nextChar(input, true);
     }
     
     char multiplicativeOperator(Scanner input) throws APException{
-    	if(!nextCharIs(input, '*', false)){
+    	if(!nextCharIs(input, '*', true)){
     		throw new APException("is not multiplication sign");
     	}
-    	return nextChar(input);
+    	return nextChar(input, true);
     }
     
     int naturalNumber(Scanner input) throws APException{
-    	return nextCharIs(input, '0', false) ? zero(input):positiveNumber(input);
+    	return nextCharIs(input, '0', true) ? zero(input):positiveNumber(input);
     }
     
     int positiveNumber(Scanner input) throws APException{
     	StringBuffer result=new StringBuffer();
-    	input.useDelimiter("");
     	result.append(notZero(input));
-    	while(nextCharIsDigit(input)){
+    	while(nextCharIsDigit(input, false)){
     		result.append(number(input));
     	}
     	return Integer.parseInt(result.toString());
     }
     
     int number(Scanner input) throws APException{
-    	return nextCharIs(input, '0', false) ? zero(input) : notZero(input);
+    	return nextCharIs(input, '0' ,false) ? zero(input) : notZero(input);
     }
     
     int zero(Scanner input) throws APException{
     	if(!nextCharIs(input, '0', false)){
     		throw new APException("is not zero");
     	}
-    	return Character.getNumericValue(nextChar(input));
+    	return Character.getNumericValue(nextChar(input, false));
     }
     
 	int notZero(Scanner input) throws APException{
 		if(!input.hasNext("[1-9]")){
 			throw new APException("is not value bewteen 1-9");
 		}
-		return Character.getNumericValue(nextChar(input));
+		return Character.getNumericValue(nextChar(input, false));
 	}
     
     char letter(Scanner input) throws APException{
-    	if(!nextCharIsLetter(input, true)){
+    	if(!nextCharIsLetter(input, false)){
 			throw new APException("is not a letter");
 		}
-    	return nextChar(input);
+    	return nextChar(input, false);
     }
     
     private void start() {
@@ -234,7 +237,9 @@ public class Main {
     	try{
     		program(programScanner);
     	}catch(APException e){
-    		throw new Error(e);
+//    		throw new Error(e);
+    		System.out.println(e+"\n");
+    		start();
     	}
     }
     
